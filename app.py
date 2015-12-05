a625a3d4a9554246e90c9de6760ea598a3140447
xtornasol512 <xtornasol512@gmail.com>
2015-12-05 07:15:43 -0600

    Templates con Jinja2

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 app.debug = True
+
 
 @app.route("/")
 def hello():
@@ -10,14 +13,22 @@ def hello():
 def rock():
     return "Rocking With Routing!!"
 
+@app.route('/hola/')
 @app.route('/hola/<nombre>')
-def hello_name(nombre):
+def hello_name(nombre='Mundo'):
     return 'Hey hola %s !!' % nombre
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_template(name=None):
+    return render_template('hello.html', name=name)
+
+
 
 
 if __name__ == "__main__":
