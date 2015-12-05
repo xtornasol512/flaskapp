804a859e31b393f5d704093db4b9728ce3de6bf3
xtornasol512 <xtornasol512@gmail.com>
2015-12-05 06:55:25 -0600

    Agregando variables y reglas de URLs

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ app.debug = True
 def hello():
     return "Hello World with FLask!"
 
-@app.route("/LetsRock")
+@app.route("/LetsRock/")
 def rock():
     return "Rocking With Routing!!"
 
+@app.route('/hola/<nombre>')
+def hello_name(nombre):
+    return 'Hey hola %s !!' % nombre
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
 if __name__ == "__main__":
